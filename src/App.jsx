fd80e2aac48a12355fbbdef747a32c94a33f6f47
Gaurav Kumar Singh <gaurav676590@gmail.com>
2024-04-15 13:50:19 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
 import Languages from './component/Languages';
 import Message from './component/Message';
+import Counter from './component/Counter';
+import ChangeName from './component/ChangeName';
+import React, { useState } from 'react'
+import ControlledName from './component/ControlledName';
+import ControlledInfo from './component/ControlledInfo';
+
 
 function App() {
+  // Lifting of State
+  // const [name, setName] = useState("JAI SHREE RAM");
+  const [name, setName] = useState("KIET");
+
 
   return (
-    <> 
-      <Message/>
-      <Languages/>
+    <>
+      {/* <Message/>
+      <Languages/> */}
+      {/* <Counter/> */}
+      {/* <ChangeName name={name} onUpdate={()=>setName("HAR HAR MAHADEV")}/> */}
+      <ControlledName name={name} onUpdate={()=>setName("MCA")}/>
+      {/* <ControlledInfo/> */}
     </>
   );
 }
--- src/component/Languages.jsx
+++ src/component/Languages.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 const Languages = () => {
-    let lang=["JAVA","JAVASCRIPT",'KOTLIN',"PYTHON","C"];
+    let lang=[{id:1,name:"JAVA"},{id:2,name:"JAVASCRIPT"},{id:3,name:'KOTLIN'},{id:4,name:"PYTHON"},{id:5,name:"C"}];
+    var check=true;
   return (
     <div>
-      <h1>Best Programming Languages:</h1>
+      <h1 className='{check?"bg-warning:"bg-success"}'>
+        {check?"Best Programming Languages:":"Top 5 Programming Languages"}</h1>
       <ul>
-        {lang.map((item,index)=>(
-            <li key={index}>{item}</li>
+        {lang.map((item)=>(
+            <li key={item.id}>{item.name}</li>
         ))}
       </ul>
+    
     </div>
   )
 }

0760ecb26f6f3985ac17f25877540f592eaacd45
Gaurav Kumar Singh <gaurav676590@gmail.com>
2024-04-16 14:54:01 +0530

    Day-9

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,27 @@ import ChangeName from './component/ChangeName';
 import React, { useState } from 'react'
 import ControlledName from './component/ControlledName';
 import ControlledInfo from './component/ControlledInfo';
+import MyCounter from './component/MyCounter';
+import MyCard from './component/MyCard';
 
 
 function App() {
-  // Lifting of State
+  // Lifting of State - when state remove from child component and manage by parent component 
+  //Central management control tool = Redux
   // const [name, setName] = useState("JAI SHREE RAM");
-  const [name, setName] = useState("KIET");
+  // const [name, setName] = useState("KIET");
 
 
   return (
     <>
       {/* <Message/>
       <Languages/> */}
-      <Counter/>
       {/* <ChangeName name={name} onUpdate={()=>setName("HAR HAR MAHADEV")}/> */}
       {/* <ControlledName name={name} onUpdate={()=>setName("MCA")}/> */}
       {/* <ControlledInfo/> */}
+      {/* <Counter/> */}
+      <MyCounter/>
+      {/* <MyCard/> */}
     </>
   );
 }
